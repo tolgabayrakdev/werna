f7e1189a448cdd7605ec58691da895c61ac135f8
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-04-23 14:46:22 +0300

    feat: sign-in and sign-up pages added router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,12 +5,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router';
 
 //pages
 import Home from './pages/home';
+import SignIn from './pages/auth/sign-in';
+import SignUp from './pages/auth/sign-up';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
+  {
+    path: '/sign-in',
+    element: <SignIn />,
+  },
+  {
+    path: '/sign-up',
+    element: <SignUp />,
+  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
