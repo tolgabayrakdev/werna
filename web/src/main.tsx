a6ebc49d7703d3324ab69005f3d9021a2a54d553
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-04-23 14:39:44 +0300

    feat: web reactjs vite project added

---
+++ web/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+
+//pages
+import Home from './pages/home'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
